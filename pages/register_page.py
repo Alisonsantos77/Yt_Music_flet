29677cad611dc1bde2d12de7017e8cb3681fff7c
Alisonsantos77 <Alisondev77@hotmail.com>
2024-09-05 14:58:00 -0300

    feat: configurar layout do painel de administração- Adicionado layout responsivo com a barra lateral e o conteudo principal.- Configurada a coluna da barra lateral para dispositivos maiores.- Ajustado espaçamento e expansçao dos elementos na pagina.

--- pages/register_page.py
+++ pages/register_page.py
@@ -1,6 +1,9 @@
 import flet as ft
 from utils.validations import validate_register
-from services import Commit
+from services import Commit, Fetch
+from flet.security import encrypt
+from config import SECRET_KEY
+from loguru import logger
 
 
 def RegisterPage(page: ft.Page):
@@ -9,7 +12,7 @@ def RegisterPage(page: ft.Page):
     def generator_avatar(e):
         if not input_username.value:
             new_avatar.current.foreground_image_src = "https://robohash.org/userdefault.png"
-            new_avatar.current.content = ft.Text("U", size=30, weight=ft.FontWeight.BOLD)
+            new_avatar.current.content = ft.Text("A", size=30, weight=ft.FontWeight.BOLD)
         else:
             new_avatar.current.foreground_image_src = f"https://robohash.org/{input_username.value}.png"
             new_avatar.current.content = ft.Text(input_username.value[0].upper(), size=30, weight=ft.FontWeight.BOLD)
@@ -20,7 +23,7 @@ def RegisterPage(page: ft.Page):
             controls=[
                 ft.CircleAvatar(
                     foreground_image_src="https://robohash.org/userdefault.png",
-                    content=ft.Text("U", size=30, weight=ft.FontWeight.BOLD),
+                    content=ft.Text("A", size=30, weight=ft.FontWeight.BOLD),
                     width=100,
                     height=100,
                     ref=new_avatar
@@ -33,36 +36,35 @@ def RegisterPage(page: ft.Page):
     def login_redirect(e):
         page.go('/login')
 
-    # Campos de entrada de dados
     input_username = ft.TextField(
         hint_text='Insira seu nome de usuário',
         col={"sm": 8, "md": 12, "xl": 12},
         on_change=generator_avatar,
-        border="underline",
+        border=ft.InputBorder.UNDERLINE,
         max_lines=1,
         max_length=20,
     )
     input_email = ft.TextField(
         hint_text='Insira seu email',
         col={"sm": 8, "md": 12, "xl": 12},
-        border="underline",
+        border=ft.InputBorder.UNDERLINE,
         max_lines=1,
         max_length=20,
     )
 
     input_senha = ft.TextField(
-        hint_text='Insira sua senha',
+        hint_text='Crie uma senha',
         password=True,
-        border="underline",
+        border=ft.InputBorder.UNDERLINE,
         max_lines=1,
         max_length=20,
         can_reveal_password=True,
         col={"sm": 8, "md": 12, "xl": 12},
     )
     input_senha_confirm = ft.TextField(
-        hint_text='Repita sua senha',
+        hint_text='Confirme sua senha',
         password=True,
-        border="underline",
+        border=ft.InputBorder.UNDERLINE,
         max_lines=1,
         max_length=20,
         can_reveal_password=True,
@@ -86,42 +88,51 @@ def RegisterPage(page: ft.Page):
         password = input_senha.value
         confirm_password = input_senha_confirm.value
 
-        # Validando os campos de registro
+        # Logando início do processo de registro
+        logger.info(f"Tentativa de registro para o usuário: {username}")
+
+        # Validação dos campos
         is_valid, error_message = validate_register(username, email, password, confirm_password)
-        print(f"Validando registro: is_valid={is_valid}, error_message={error_message}")
         if not is_valid:
-            page.snack_bar = ft.SnackBar(content=ft.Text(error_message))
+            logger.warning(f"Falha na validação do registro para {username}: {error_message}")
+            page.snack_bar = ft.SnackBar(content=ft.Text(error_message))  # Usa a mensagem humanizada
             page.snack_bar.open = True
             page.update()
             return
 
         try:
-            # Commit da solicitação de registro na tabela `registration_requests`
+            # Criptografando a senha
+            encrypted_password = encrypt(password, SECRET_KEY)
+            logger.info(f"Senha criptografada para {username}")
+
+            # Inserindo o usuário na tabela `registration_requests` com status 'pending'
             Commit.commit_registration_request(
                 data={
                     'username': username,
                     'email': email,
-                    'password': password,
-                    'status': 'pending',
+                    'password': encrypted_password,
+                    'status': 'pending',  # Define o status como 'pending'
+                    'reviewed_by_admin_id': None  # Nenhum admin revisou ainda
                 }
             )
 
+            logger.info(f"Usuário {username} registrado na tabela de requests com status 'pending'.")
+
+            # Notificando o sucesso
             page.snack_bar = ft.SnackBar(
-                content=ft.Text("Solicitação de registro enviada com sucesso! Aguarde aprovação."))
+                content=ft.Text("Cadastro enviado para revisão. Aguarde a aprovação de um administrador!"))
             page.snack_bar.open = True
             page.update()
 
-            # Navegar para a página de login após envio da solicitação
+            # Redirecionar para a página de login
             page.go('/login')
 
         except Exception as error:
-            # Tratar qualquer erro que possa ocorrer
-            print(f"Erro ao enviar solicitação de registro: {error}")
-            page.snack_bar = ft.SnackBar(content=ft.Text(f"Erro ao registrar: {str(error)}"))
+            logger.error(f"Erro ao registrar a solicitação de {username}: {error}")
+            page.snack_bar = ft.SnackBar(content=ft.Text(f"Desculpe, ocorreu um erro durante o cadastro: {str(error)}"))
             page.snack_bar.open = True
             page.update()
 
-    # Layout da página
     return ft.Container(
         image_src='https://images3.alphacoders.com/133/1332803.png',
         image_fit=ft.ImageFit.COVER,
@@ -155,7 +166,7 @@ def RegisterPage(page: ft.Page):
                                         ]
                                     ),
                                     ft.Text(
-                                        value="Bem-vindo de volta!",
+                                        value="Crie sua conta",
                                         size=20,
                                         color=ft.colors.WHITE,
                                         weight=ft.FontWeight.BOLD,
@@ -173,7 +184,7 @@ def RegisterPage(page: ft.Page):
                             input_senha,
                             input_senha_confirm,
                             ft.ElevatedButton(
-                                text="Criar conta",
+                                text="Cadastrar",
                                 style=ft.ButtonStyle(
                                     padding=ft.padding.all(10),
                                     bgcolor={
