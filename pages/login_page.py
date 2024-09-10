99841b3c98fd4bf1ea6f251d907e5256f0144fd4
Alisonsantos77 <Alisondev77@hotmail.com>
2024-09-09 22:17:42 -0300

    feat: adicionar validação de login com criptografia e feedback detalhado para administrador
    -Implementada a funcionalidade de login com validação completa dos campos de nome de usuário e senha.
    -Utilizada a função decrypt com a SECRET_KEY para descriptografar a senha armazenada no banco de dados.
    -Adicionado suporte à sessão para armazenar o ID e nome de usuário do administrador após o login.
    -Melhorada a experiência do usuário com feedback visual através de snackbar em casos de sucesso e erro (ex.: senha incorreta, usuário não encontrado).
    -Implementada a geração dinâmica de avatar com base no nome de usuário utilizando a API RoboHash.
    -Adicionado redirecionamento automático para o painel de admin após um login bem-sucedido.
    -Melhorada a organização e responsividade do layout, incluindo suporte a diferentes tamanhos de tela.

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,6 +4,7 @@ from services import Fetch, Commit
 from flet.security import decrypt
 from config import SECRET_KEY
 from loguru import logger
+from datetime import datetime, timezone
 
 
 def LoginPage(page: ft.Page):
@@ -40,11 +41,9 @@ def LoginPage(page: ft.Page):
     # Campos de entrada de dados
     input_username = ft.TextField(
         hint_text='Insira seu nome de usuário',
-        col={"sm": 8, "md": 12, "xl": 12},
+        col={"sm": 8, "md": 12, "xl": 5},
         on_change=generator_avatar,
         border=ft.InputBorder.UNDERLINE,
-        max_lines=1,
-        max_length=20,
     )
 
     input_senha = ft.TextField(
@@ -54,7 +53,7 @@ def LoginPage(page: ft.Page):
         max_lines=1,
         max_length=20,
         can_reveal_password=True,
-        col={"sm": 8, "md": 12, "xl": 12},
+        col={"sm": 8, "md": 12, "xl": 5},
     )
 
     password_reset_btn = ft.TextButton(
@@ -67,42 +66,58 @@ def LoginPage(page: ft.Page):
         text='Criar nova conta',
         col={"sm": 8, "md": 12, "xl": 12},
         style=ft.ButtonStyle(
-            color=ft.colors.BLACK,
+            color={
+                ft.ControlState.DEFAULT: ft.colors.WHITE,
+                ft.ControlState.HOVERED: ft.colors.BLACK,
+            },
             elevation=2,
             overlay_color=ft.colors.TRANSPARENT
         ),
         on_click=register_redirect
     )
 
-    # Função para tratar o evento de login
-
     def handle_login(e):
         username = input_username.value
         password = input_senha.value
 
-        # Validação dos campos de login
         is_valid, error_message = validate_login(username, password)
         if not is_valid:
             logger.warning(f"Falha na validação do login para {username}: {error_message}")
-            # Exibindo a mensagem retornada pelo validate_login
-            page.snack_bar = ft.SnackBar(content=ft.Text(error_message))
+            page.snack_bar = ft.SnackBar(content=ft.Text(error_message, size=20,
+                                                         color=ft.colors.RED,
+                                                         weight=ft.FontWeight.W_600,
+                                                         italic=True))
             page.snack_bar.open = True
             page.update()
             return
 
         try:
-            # Verificar se o admin existe
-            admin = Fetch.fetch_admin_by_username(username)
+            user = Fetch.fetch_user_by_username(username)
 
-            if admin is not None:
-                decrypted_admin_password = decrypt(admin['password'], SECRET_KEY)
+            if user is not None:
+                try:
+                    encrypted_password = user['password']
+                    stored_password = decrypt(encrypted_password, SECRET_KEY)
+                except Exception as ex:
+                    logger.error(f"Erro ao descriptografar a senha para o usuário {username}: {ex}")
+                    page.snack_bar = ft.SnackBar(content=ft.Text("Erro na autenticação. Tente novamente.", size=20,
+                                                                 color=ft.colors.RED,
+                                                                 weight=ft.FontWeight.W_600,
+                                                                 italic=True))
+                    page.snack_bar.open = True
+                    page.update()
+                    return
 
-                if decrypted_admin_password == password:
-                    # Armazenar o ID do admin na sessão
-                    page.session.set("admin_id", admin['id'])
-                    page.session.set("admin_username", admin['username'])
+                # Compara a senha inserida pelo usuário com a senha armazenada
+                if stored_password == password:
+                    page.session.set("user_id", user['id'])
+                    page.session.set("user_username", user['username'])
+                    logger.info(f"Login bem-sucedido para o usuário: {username}")
 
-                    logger.info(f"Login bem-sucedido para o administrador: {username}, ID: {admin['id']}")
+                    # Atualiza o campo last_login no banco de dados
+                    last_login_time = datetime.now(timezone.utc).isoformat()
+                    Commit.update_last_login(user['id'], last_login_time)
+                    logger.info(f"last_login atualizado para o usuário {username}")
 
                     page.snack_bar = ft.SnackBar(
                         content=ft.Text(f'Bem-vindo de volta, {username}!', size=20, color=ft.colors.BLUE,
@@ -110,23 +125,24 @@ def LoginPage(page: ft.Page):
                     )
                     page.snack_bar.open = True
                     page.update()
-                    page.go('/admin')
+
+                    page.go('/home')
                 else:
-                    logger.warning(f"Senha incorreta para o administrador: {username}")
-                    page.snack_bar = ft.SnackBar(content=ft.Text("Senha incorreta. Tente novamente."))
+                    logger.warning(f"Tentativa de login falhou para {username}: senha incorreta")
+                    page.snack_bar = ft.SnackBar(content=ft.Text("Usuário ou senha incorretos. Tente novamente."))
                     page.snack_bar.open = True
                     page.update()
             else:
                 logger.warning(f"Usuário {username} não encontrado")
-                page.snack_bar = ft.SnackBar(content=ft.Text("Usuário não encontrado. Verifique o nome de usuário."))
+                page.snack_bar = ft.SnackBar(content=ft.Text("Usuário ou senha incorretos. Tente novamente."))
                 page.snack_bar.open = True
                 page.update()
 
         except Exception as ex:
-            logger.error(f"Erro ao efetuar login para o usuário {username}: {ex}")
+            logger.error(f"Erro ao processar o login para {username}: {ex}")
             page.snack_bar = ft.SnackBar(
                 content=ft.Text(
-                    value=f"Erro ao efetuar login: {ex}",
+                    value=f"Erro ao efetuar login. Tente novamente.",
                     size=20,
                     color=ft.colors.RED,
                     weight=ft.FontWeight.W_600
@@ -135,19 +151,14 @@ def LoginPage(page: ft.Page):
             page.snack_bar.open = True
             page.update()
 
-
-    # Layout da página
     return ft.Container(
-        image_src='https://images3.alphacoders.com/133/1332803.png',
-        image_fit=ft.ImageFit.COVER,
         expand=True,
         alignment=ft.alignment.center,
         content=ft.Container(
-            padding=ft.padding.symmetric(vertical=50, horizontal=80),
+            padding=ft.padding.symmetric(vertical=20, horizontal=40),
             border_radius=ft.border_radius.all(10),
             blur=ft.Blur(sigma_x=8, sigma_y=8),
-            height=600,
-            aspect_ratio=9 / 16,
+            margin=20,
             bgcolor=ft.colors.with_opacity(0.1, ft.colors.WHITE),
             border=ft.Border(
                 top=ft.BorderSide(width=2, color=ft.colors.WHITE30),
@@ -172,6 +183,7 @@ def LoginPage(page: ft.Page):
                                     ),
                                     ft.Text(
                                         value="Bem-vindo de volta!",
+                                        overflow=ft.TextOverflow.ELLIPSIS,
                                         size=20,
                                         color=ft.colors.WHITE,
                                         weight=ft.FontWeight.BOLD,
@@ -183,6 +195,7 @@ def LoginPage(page: ft.Page):
                     ),
                     ft.ResponsiveRow(
                         col={"sm": 10, "md": 12, "xl": 12},
+                        alignment=ft.MainAxisAlignment.CENTER,
                         controls=[
                             input_username,
                             input_senha,
@@ -197,17 +210,20 @@ def LoginPage(page: ft.Page):
                                 style=ft.ButtonStyle(
                                     padding=ft.padding.all(10),
                                     bgcolor={
-                                        ft.MaterialState.HOVERED: ft.colors.PRIMARY,
+                                        ft.ControlState.HOVERED: ft.colors.WHITE,
                                     },
                                     color={
-                                        ft.MaterialState.DEFAULT: ft.colors.WHITE,
-                                        ft.MaterialState.HOVERED: ft.colors.BLACK,
-
-                                    }
+                                        ft.ControlState.DEFAULT: ft.colors.WHITE,
+                                        ft.ControlState.HOVERED: ft.colors.BLACK,
+                                    },
+                                    elevation={"pressed": 0, "": 1},
+                                    animation_duration=500,
+                                    shape=ft.RoundedRectangleBorder(radius=6),
                                 ),
-                                on_click=handle_login
+                                on_click=handle_login,
+                                col={"sm": 8, "md": 10, "xl": 6},
                             ),
-                            ft.Row(
+                            ft.ResponsiveRow(
                                 alignment=ft.MainAxisAlignment.CENTER,
                                 controls=[
                                     new_account_btn
